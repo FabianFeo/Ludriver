6ce39140b9842e97e00fefa4db3c342fe70f858f
sergioe.riverosr <sergioe.riverosr@konradlorenz.edu.co>
2021-03-24 11:54:45 -0500

    drivers android  funcionales

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:luconductora/src/service/faceNetService.dart';
+import 'package:luconductora/src/service/mlVisionService.dart';
 import 'package:luconductora/src/view/Carga.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +13,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Firebase.initializeApp().then((value) async {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
+
+      FaceNetService _faceNetService = FaceNetService();
+      MLVisionService _mlVisionService = MLVisionService();
+      await _faceNetService.loadModel();
+
+      _mlVisionService.initialize();
+    });
     return MaterialApp(
       title: 'Lü conductoras',
       theme: ThemeData(
