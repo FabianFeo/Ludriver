10de0a23757d9f4565d9918ade2a35b56fa9aee3
EyeWithItHandy <64708166+EyeWithItHandy@users.noreply.github.com>
2021-04-04 04:51:45 -0500

    Visual y boton panico

---
+++ lib/src/view/BotonPanicoDriver.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:luconductora/src/view/LuDriverAlarmada.dart';
+import 'package:luconductora/src/view/LuDriverPreocupada.dart';
+
+class BotonPanicoDriver extends StatefulWidget {
+  BotonPanicoDriver({Key key}) : super(key: key);
+
+  @override
+  _BotonPanicoDriverState createState() => _BotonPanicoDriverState();
+}
+
+class _BotonPanicoDriverState extends State<BotonPanicoDriver> {
+  double height = 0;
+  double width = 0;
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: height / 8),
+                    child: Center(
+                      child: Text(
+                        '¿Te sientes insegura?',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: height / 25,
+                            color: Color.fromRGBO(101, 79, 168, 1),
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: height / 30),
+                    child: Center(
+                      child: Text(
+                        'Lü te ayuda, reporta tú tipo de emergencia y como te sientes para poder atender mejor tu emergencia.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: height / 50,
+                            color: Color.fromRGBO(101, 79, 168, 1),
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: width / 1.5,
+                    child: Container(
+                      child: Image(
+                        image: AssetImage('assets/Logo/Usuaria.png'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: height / 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            GestureDetector(
+                              child: Container(
+                                width: width / 3.5,
+                                child: Container(
+                                  child: Image(
+                                    image:
+                                        AssetImage('assets/IconosMarker/Lu_yellow.png'),
+                                  ),
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => LuDriverAlarmada()));
+                              },
+                            ),
+                            Container(
+                              child: Text(
+                                'Lü alarmada',
+                                style: TextStyle(
+                                    fontSize: height / 50,
+                                    color: Colors.yellow,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            )
+                          ],
+                        ),                       
+                        Column(
+                          children: [
+                            GestureDetector(
+                              child: Container(
+                                width: width / 3.5,
+                                child: Container(
+                                  child: Image(
+                                    image:
+                                        AssetImage('assets/IconosMarker/Lu_red.png'),
+                                  ),
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => LudriverPreocupada()));
+                              },
+                            ),
+                            Container(
+                              child: Text(
+                                'Lü asustada',
+                                style: TextStyle(
+                                    fontSize: height / 50,
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/view/Index.dart
+++ lib/src/view/Index.dart
@@ -2,6 +2,8 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:luconductora/src/model/Driver.model.dart';
 import 'package:luconductora/src/view/Ayuda.dart';
+import 'package:luconductora/src/view/BlogDriver.dart';
+import 'package:luconductora/src/view/BotonPanicoDriver.dart';
 import 'package:luconductora/src/view/Carga.dart';
 import 'package:luconductora/src/view/Configuracion.dart';
 import 'package:luconductora/src/view/Contactoconfianza.dart';
@@ -79,14 +81,14 @@ class _IndexState extends State<Index> {
             setState(() {
               switch (index) {
                 case 0:
-                  pageCurrent = Container();
+                  pageCurrent = BlogDriver();
                   break;
                 case 1:
                   pageCurrent = MapaPage();
 
                   break;
                 case 2:
-                  pageCurrent = Container();
+                  pageCurrent = BotonPanicoDriver();
                   break;
               }
               _page = index;
