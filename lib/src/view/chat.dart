bd0ab9366231fb36c84795899466a95b5a139228
juyliantamayo <juliantamayo16@outlook.es>
2021-04-14 20:13:54 -0500

    chat

--- lib/src/view/chat.dart
+++ lib/src/view/chat.dart
@@ -81,56 +81,60 @@ class _ChatState extends State<Chat> {
               backgroundColor: Colors.purple,
             ),
             body: SingleChildScrollView(
+              
                 child: Stack(children: <Widget>[
-              StreamBuilder(
-                  stream: chatService.getMessageDriver(travel),
-                  builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasData && snapshot.data.size != 0) {
-                      messages.clear();
-                      snapshot.data.docs.forEach((element) {
-                        messages.add(ChatMessage(
-                            messageContent: element['messageDriver'] == null
-                                ? element['messageUser']
-                                : element['messageDriver'],
-                            messageType: element['messageDriver'] == null
-                                ? 'receiver'
-                                : 'sender'));
-                      });
-                    }
-                    return ListView.builder(
-                      itemCount: messages.length,
-                      shrinkWrap: true,
-                      padding: EdgeInsets.only(top: 10, bottom: 10),
-                      physics: NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return Container(
-                          padding: EdgeInsets.only(
-                              left: 14, right: 14, top: 10, bottom: 10),
-                          child: Align(
-                            alignment:
-                                (messages[index].messageType == "receiver"
-                                    ? Alignment.topLeft
-                                    : Alignment.topRight),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color:
-                                    (messages[index].messageType == "receiver"
-                                        ? Colors.grey.shade200
-                                        : Colors.deepPurple[200]),
-                              ),
-                              padding: EdgeInsets.all(16),
-                              child: Text(
-                                messages[index].messageContent,
-                                style: TextStyle(fontSize: 15),
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  }),
-              _buildMessageComposer()
-            ]))));
+                  SingleChildScrollView(
+                    child: StreamBuilder(
+                        stream: chatService.getMessageDriver(travel),
+                        builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
+                          if (snapshot.hasData && snapshot.data.size != 0) {
+                            messages.clear();
+                            snapshot.data.docs.forEach((element) {
+                              messages.add(ChatMessage(
+                                  messageContent:
+                                      element['messageDriver'] == null
+                                          ? element['messageUser']
+                                          : element['messageDriver'],
+                                  messageType: element['messageDriver'] == null
+                                      ? 'receiver'
+                                      : 'sender'));
+                            });
+                          }
+                          return ListView.builder(
+                            itemCount: messages.length,
+                            shrinkWrap: true,
+                            padding: EdgeInsets.only(top: 10, bottom: 10),
+                            physics: NeverScrollableScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              return Container(
+                                padding: EdgeInsets.only(
+                                    left: 14, right: 14, top: 10, bottom: 10),
+                                child: Align(
+                                  alignment:
+                                      (messages[index].messageType == "receiver"
+                                          ? Alignment.topLeft
+                                          : Alignment.topRight),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: (messages[index].messageType ==
+                                              "receiver"
+                                          ? Colors.grey.shade200
+                                          : Colors.deepPurple[200]),
+                                    ),
+                                    padding: EdgeInsets.all(16),
+                                    child: Text(
+                                      messages[index].messageContent,
+                                      style: TextStyle(fontSize: 15),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }),
+                  ),
+                  _buildMessageComposer()
+                ]))));
   }
 }
